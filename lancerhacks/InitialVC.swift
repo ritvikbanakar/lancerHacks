c9a547d455cea121011ced9c11e4e37dd0c1a696
Ritvik <ritvikbanakar15@gmail.com>
2021-02-27 17:46:43 -0800

    first

---
+++ lancerhacks/InitialVC.swift
@@ -0,0 +1,29 @@
+//
+//  InitialVC.swift
+//  lancerhacks
+//
+//  Created by Vidit Agrawal on 2/27/21.
+//
+
+import UIKit
+
+class InitialVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ lancerhacks/Utilities.swift
@@ -0,0 +1,8 @@
+//
+//  Utilities.swift
+//  lancerhacks
+//
+//  Created by Vidit Agrawal on 2/27/21.
+//
+
+import Foundation
